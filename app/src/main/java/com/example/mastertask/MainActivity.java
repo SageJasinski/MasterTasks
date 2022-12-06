8486da1639bfe76b0dbaeaf8c62de2326612ca1c
Sage Jasinski <sagejasinski@gmail.com>
2022-12-05 21:31:01 -0800

    Remake of the broken app

--- app/src/main/java/com/example/mastertask/MainActivity.java
+++ app/src/main/java/com/example/mastertask/MainActivity.java
@@ -6,8 +6,12 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import androidx.preference.PreferenceManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,11 +21,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button btn = (Button)findViewById(R.id.button_addTask);
+        ImageButton imgBtn = findViewById(R.id.main_user_setting_btn);
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+
+//        RecyclerView.LayoutManager
 
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, TaskAdd.class);
+                startActivity(intent);
+            }
+        });
+
+
+        imgBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, UserProfile.class);
                 startActivity(intent);
             }
         });
